536b85b9bfd90f6b2b433d4393c1ce1803dc40e3
Akshay Singh <akshaysingh@Akshays-MacBook-Pro.local>
2015-09-05 17:32:27 -0400

    Color badal gaya

--- animals.py
+++ animals.py
@@ -5,15 +5,13 @@ from tkinter import *
 
 
 
-
-
 def main_window():
     window = Tk()
     window.geometry("600x300")
     window.config(background="light green")
 
     water = Frame(window, height=200, width=100)
-    water.config(background="light blue")
+    water.config(background="blue")
     water.pack(padx=50, pady=50)
     window.mainloop()
 
---
+++ new.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env/ python3
+
+__author__ = 'akshaysingh'
+
+
+
+
+from tkinter import *
+
