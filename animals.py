b96dc834f3a25b78ed04f9190a8a22fba204bf29
jaistudy1996 <jaistudy1996@gmail.com>
2015-09-05 17:11:01 -0400

    Initial commit

---
+++ animals.py
@@ -0,0 +1,23 @@
+__author__ = 'JayantArora'
+
+
+from tkinter import *
+
+
+
+
+
+def main_window():
+    window = Tk()
+    window.geometry("600x300")
+    window.config(background="light green")
+
+    water = Frame(window, height=200, width=100)
+    water.config(background="light blue")
+    water.pack(padx=50, pady=50)
+    window.mainloop()
+
+
+
+
+main_window()
