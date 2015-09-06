064964786d8f4314a635fd708db1957d6d70104b
jaistudy1996 <jaistudy1996@gmail.com>
2015-09-05 23:22:15 -0400

    Objects bear and fish added

--- animals.py
+++ animals.py
@@ -2,21 +2,45 @@ __author__ = 'JayantArora'
 
 
 from tkinter import *
+from random import *
 
 
 def main_window():
     window = Tk()
-    window.geometry("600x300")
+    window.geometry("1000x1000")
     window.config(background="light green")
+    #window.
+    # water = Frame(window, height=100, width=500)
+    # water.config(background="light blue", highlightbackground="black")
+    # water.pack(padx=50, pady=50)
 
-    water = Frame(window, height=200, width=100)
-    water.config(background="blue")
-    water.pack(padx=50, pady=50)
+    canvas = Canvas(window, height="500p", width="500p", bg="white")
+
+    canvas.pack()
+
+    bears = []
+    fish = []
+
+    for i in range(100):
+        bearX = 10*random()
+        bearY = 10*random()
+        bears.append((bearX, bearY))
+        #print(bears)
+
+
+    for j in range(len(bears)):
+        x, y = bears[j]
+        print(x, y)
+        # a, b = bears[j]
+        # print(10*a, 10*b)
+        global bear
+        bear = canvas.create_oval(50*x, 50*y, (50*x)+10, (50*y)+10)
+        bear2 = canvas.create_oval(100*x, 100*y, (100*x)+10, (100*y)+10)
+
+    #for k in range(10000):
+
+
+
     window.mainloop()
-
-
-
-
-
 
 main_window()
