bd74f936e9856fdc9078df27f1f1dc90d362b65a
jaistudy1996 <jaistudy1996@gmail.com>
2015-09-07 23:56:18 -0400

    List River update

--- newIdea.py
+++ newIdea.py
@@ -1,69 +1,68 @@
 __author__ = 'JayantArora'
 
 from random import random, randint, choice
-from time import time, sleep
+from time import sleep
 
 
 def main():
 
-    bear = []
-    fish = []
+    river = []
 
     gender = ['male', 'female']
+    animal = ['bear', 'fish']
 
     for i in range(100):
-        bear.append((choice(gender), 10*random()))
-        fish.append((choice(gender), 10*random()))
+        river.append((choice(animal), choice(gender), 10*random()))
+        # 10*random() is the strength of the animal
 
-    print(bear)
-    print(fish)
-
+    print(river)
 
     while True:
-        index = randint(1, len(bear))
+        index = randint(1, len(river))
         will_change = randint(1, 4)
 
         if will_change == 1:        # if 1 then the bear stays where it is
-            bear[index] = bear[index]
+            river[index] = river[index]
+            print("stay at same place")
             sleep(3)
             print("slept 3 secs")
 
         if will_change == 2:
             # bear[index] = bear[index+1]
-            if bear[index][0] != bear[index+1][0]:
-                bear.append((choice(gender), 10*random()))
+            if river[index][1] != river[index+1][1] and river[index][0] == river[index+1][0]:
+                river.append((choice(gender), 10*random()))
                 print("Bear born")
                 sleep(3)
                 print("slept 3 secs")
 
-            if bear[index][0] == bear[index][0]:
-                if bear[index][1] > bear[index+1][1]:
-                    del(bear[index+1])
+            if river[index][1] == river[index+1][1] or river[index][0] != river[index+1][0]:
+                if river[index][2] > river[index+1][2]:
+                    del(river[index+1])
                     print("2, bear died")
                     sleep(3)
                     print("slept 3 secs")
-                if bear[index][1] < bear[index+1][1]:
-                    del(bear[index])
+                if river[index][1] < river[index+1][1]:
+                    del(river[index])
                     print("2, bear died")
                     sleep(3)
                     print("slept 3 secs")
 
         if will_change == 3:
-            if bear[index][0] != bear[index-1][0]:
-                bear.append((choice(gender), 10*random()))
+            if river[index][0] != river[index-1][0]:
+                river.append((choice(gender), 10*random()))
                 print("Bear born")
                 sleep(3)
                 print("slept 3 secs")
 
-            if bear[index][0] == bear[index][0]:
-                if bear[index][1] > bear[index-1][1]:
-                    del(bear[index-1])
+            if river[index][0] == river[index][0]:
+                if river[index][1] > river[index-1][1]:
+                    del(river[index-1])
                     print("3, bear died")
                     sleep(3)
                     print("slept 3 secs")
 
-                if bear[index][1] < bear[index-1][1]:
-                    del(bear[index])
+                if river[index][1] < river[index-1][1]:
+                    del(river[index])
                     print("3, bear died")
                     sleep(3)
                     print("slept 3 secs")
