1d3154b17cf19e4b026b6486d0be7cc5703d591c
jaistudy1996 <jaistudy1996@gmail.com>
2015-09-08 15:43:58 -0400

    Exception handling added and sleep timer decreased

--- newIdea.py
+++ newIdea.py
@@ -17,85 +17,83 @@ def main():
 
     print(river)
 
-    while True:
-        index = randint(1, len(river))
-        will_change = randint(1, 4)     # possible chances where the object will mova
-                                        # if 1: stay where it is
-                                        # if 2: move to the next location in the list
-                                        # if 3: will move to the previous location
+    try:
+        while True:
+            global index
+            index = randint(0, len(river))
+            will_change = randint(1, 4)     # possible chances where the object will mova
+                                            # if 1: stay where it is
+                                            # if 2: move to the next location in the list
+                                            # if 3: will move to the previous location
 
-        if will_change == 1:        # if 1 then the bear stays where it is
-            river[index] = river[index]
-            print("stay at same place")
-            sleep(3)
-            print("slept 3 secs")
-        ########
-        if will_change == 2:
-            # bear[index] = bear[index+1]
-            if river[index][1] != river[index+1][1] and river[index][0] == river[index+1][0]:
-                ani = choice(animal)
-                gen = choice(gender)
-                river.append((ani, gen, 10*random()))
-                print(ani+" "+gen+" born")
-                sleep(3)
-                print("slept 3 secs")
-        ########
-            if river[index][1] == river[index+1][1] and river[index][0] == river[index+1][0]:
-                if river[index][2] > river[index+1][2]:
-                    print("2, "+river[index+1][0]+" died")
-                    del(river[index+1])
-                    sleep(3)
-                    print("slept 3 secs")
-                if river[index][2] < river[index+1][2]:
-                    print("2, "+river[index][0]+" died")
-                    del(river[index])
-                    sleep(3)
-                    print("slept 3 secs")
-        ########
-            if river[index][0] != river[index+1][0]:
-                if river[index][0] == 'bear':
-                    print("Bear killed fish")
-                    del(river[index+1])
-                    sleep(3)
-                    print("slept 3 seconds")
-                else:
-                    print("bear killed fish")
-                    del(river[index])
-                    sleep(3)
-                    print("slept 3 seconds")
-        ########
+            if will_change == 1:        # if 1 then the bear stays where it is
+                river[index] = river[index]
+                print("stay at same place")
+                sleep(1)
+            ########
+            if will_change == 2:
+                # bear[index] = bear[index+1]
+                if river[index][1] != river[index+1][1] and river[index][0] == river[index+1][0]:
+                    ani = choice(animal)
+                    gen = choice(gender)
+                    river.append((ani, gen, 10*random()))
+                    print(ani+" "+gen+" born")
+                    sleep(1)
+            ########
+                if river[index][1] == river[index+1][1] and river[index][0] == river[index+1][0]:
+                    if river[index][2] > river[index+1][2]:
+                        print("2, "+river[index+1][0]+" "+river[index][1]+" died")
+                        del(river[index+1])
+                        sleep(1)
+                    if river[index][2] < river[index+1][2]:
+                        print("2, "+river[index][0]+" "+river[index][1]+" died")
+                        del(river[index])
+                        sleep(1)
+            ########
+                if river[index][0] != river[index+1][0]:
+                    if river[index][0] == 'bear':
+                        print("Bear killed fish")
+                        del(river[index+1])
+                        sleep(1)
+                    else:
+                        print("bear killed fish")
+                        del(river[index])
+                        sleep(1)
+            ########
 
-        if will_change == 3:
-            if river[index][1] != river[index-1][1] and river[index][0] == river[index-1][0]:
-                ani = choice(animal)
-                gen = choice(gender)
-                river.append((ani, gen, 10*random()))
-                print(ani+" "+gen+" born")
-                sleep(3)
-                print("slept 3 secs")
-        ########
-            if river[index][1] == river[index-1][1] and river[index][0] == river[index-1][0]:
-                if river[index][2] > river[index-1][2]:
-                    print("2, "+river[index-1][0]+" died")
-                    del(river[index-1])
-                    sleep(3)
-                    print("slept 3 secs")
-                if river[index][2] < river[index-1][2]:
-                    print("2, "+river[index][0]+" died")
-                    del(river[index])
-                    sleep(3)
-                    print("slept 3 secs")
-        ########
-            if river[index][0] != river[index-1][0]:
-                if river[index][0] == 'bear':
-                    print("Bear killed fish")
-                    del(river[index-1])
-                    sleep(3)
-                    print("slept 3 seconds")
-                else:
-                    print("Bear killed fish")
-                    del(river[index])
-                    sleep(3)
-                    print("slept 3 seconds")
+            if will_change == 3:
+                if river[index][1] != river[index-1][1] and river[index][0] == river[index-1][0]:
+                    ani = choice(animal)
+                    gen = choice(gender)
+                    river.append((ani, gen, 10*random()))
+                    print(ani+" "+gen+" born")
+                    sleep(1)
+            ########
+                if river[index][1] == river[index-1][1] and river[index][0] == river[index-1][0]:
+                    if river[index][2] > river[index-1][2]:
+                        print("3, "+river[index-1][0]+" "+river[index-1][1]+" died")
+                        del(river[index-1])
+                        sleep(1)
+                    if river[index][2] < river[index-1][2]:
+                        print("3, "+river[index][0]+" "+river[index-1][1]+" died")
+                        del(river[index])
+                        sleep(1)
+            ########
+                if river[index][0] != river[index-1][0]:
+                    if river[index][0] == 'bear':
+                        print("Bear killed fish")
+                        del(river[index-1])
+                        sleep(1)
+                    else:
+                        print("Bear killed fish")
+                        del(river[index])
+                        sleep(1)
+
+    except IndexError:
+        print("Error:", index)
+
+    except KeyboardInterrupt:
+        print("Ecosystem's life stopped!")
+
 
 main()
