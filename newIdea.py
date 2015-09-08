8e7f87a1c8dab777112a0325dbf72bb9257e2c59
jaistudy1996 <jaistudy1996@gmail.com>
2015-09-08 16:02:30 -0400

    logs added

--- newIdea.py
+++ newIdea.py
@@ -1,11 +1,11 @@
 __author__ = 'JayantArora'
 
 from random import random, randint, choice
-from time import sleep
+from time import sleep, time
 
 
 def main():
-
+    start = time()
     river = []
 
     gender = ['male', 'female']
@@ -15,22 +15,23 @@ def main():
         river.append((choice(animal), choice(gender), 10*random()))
         # 10*random() is the strength of the animal
 
-    print(river)
-
     try:
         while True:
-            global index
-            index = randint(0, len(river))
+            index = randint(0, len(river)-1)
             will_change = randint(1, 4)     # possible chances where the object will mova
                                             # if 1: stay where it is
                                             # if 2: move to the next location in the list
                                             # if 3: will move to the previous location
 
-            if will_change == 1:        # if 1 then the bear stays where it is
-                river[index] = river[index]
-                print("stay at same place")
-                sleep(1)
-            ########
+            try:
+                if will_change == 1:        # if 1 then the bear stays where it is
+                    river[index] = river[index]
+                    print("stay at same place")
+                    sleep(1)
+                ########
+            except IndexError:
+                pass
+
             if will_change == 2:
                 # bear[index] = bear[index+1]
                 if river[index][1] != river[index+1][1] and river[index][0] == river[index+1][0]:
@@ -89,11 +90,12 @@ def main():
                         del(river[index])
                         sleep(1)
 
-    except IndexError:
-        print("Error:", index)
-
     except KeyboardInterrupt:
-        print("Ecosystem's life stopped!")
+        print("\nEcosystem's life stopped!")
+        stop = time()
+        time_run = stop-start
+        log = ["The program ran for:", time_run, "Stopped at index: ", index, will_change]
+        print(log)
 
 
 main()
